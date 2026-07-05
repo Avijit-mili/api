d8cf6e170e63c8c8e488450279c01f788d37c8f6
Avijit-mili <ytavi283@gmail.com>
2026-07-05 23:14:13 +0530

    Update activate.php

--- activate.php
+++ activate.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=utf-8");
 $inputKey = $_GET['key'] ?? '';
 
 // ২. আপনার নিজের তৈরি করা সিক্রেট Key (এটি আপনি অ্যাপে বা লিংকে ব্যবহার করবেন)
-$mySecretKey = "venonmengine"; 
+$mySecretKey = "Zoroo-On-TOP"; 
 
 // ৩. যাচাই করার লজিক
 if ($inputKey === $mySecretKey) {
